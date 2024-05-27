1c63e54fe6348d510a1e1fc2bbe9177c4e444035
david <daviddzura@gmail.com>
2024-05-27 22:31:10 +0200

    Revert "Revert "initial commit""
    This reverts commit 4d070ca188a3ccd1f8f566e23f9f6feb13a39dc2.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import NeoPage from "./NeoPage.tsx";
+
+function App() {
+	return (
+		<main className='flex w-screen justify-center'>
+			<div className='bg'/>
+			<NeoPage/> 
+		</main>
+	)
+}
+
+export default App
