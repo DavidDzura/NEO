6d9f8a21feacb49fd700d7644979308efeec9ce4
david <daviddzura@gmail.com>
2024-05-27 21:59:38 +0200

    initial commit

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,10 @@
+
+export const Spinner = () => {
+    return (
+        <div className='spinner'>
+            <div className='bounce1'/>
+            <div className='bounce2'/>
+            <div className='bounce3'/>
+        </div>
+    );
+};
