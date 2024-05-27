4d070ca188a3ccd1f8f566e23f9f6feb13a39dc2
david <daviddzura@gmail.com>
2024-05-27 22:18:07 +0200

    Revert "initial commit"
    This reverts commit 6d9f8a21feacb49fd700d7644979308efeec9ce4.

--- frontend/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export const Header = () => {
-	return (
-		<div className='flex flex-col items-center'>
-			<h1 className='mt-10 mb-5 text-center mx-5'>Near-Earth Objects</h1>
-			<h3>Please select date with max range of 7 days.</h3>
-		</div>
-	);
-};
